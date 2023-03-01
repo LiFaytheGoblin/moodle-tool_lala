f25c9cd6e0a8d4f331b3b2def17727bade2b838d
Linda Fernsel <linda.fernsel@htw-berlin.de>
2023-03-01 11:39:12 +0000

    adds stub for model_configuration class

---
+++ classes/model_configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace tool_laaudit;
+
+// file admin/tool/laaudit/classes/model_configuration.php
+class model_configuration {
+    private $ID = 100000;
+
+    function get_id() {
+        return $this->ID;
+    }
+}

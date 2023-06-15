56cb9d166e51808f9ecaf1e06c1d8152640caf92
Linda Fernsel <linda.fernsel@htw-berlin.de>
2023-06-15 13:31:32 +0200

    adds error case

--- tests/model_configuration_get_or_create_and_get_for_model_test.php
+++ tests/model_configuration_get_or_create_and_get_for_model_test.php
@@ -62,26 +62,46 @@ class model_configuration_get_or_create_and_get_for_model_test extends \advanced
         // Get a valid model id but no existing model config.
         $validmodelid2 = $DB->insert_record('analytics_models', $validmodelobject);
 
+
+
         // Get a model id and model config, then delete the model id.
         $tobedeletedmodelid = $DB->insert_record('analytics_models', $validmodelobject);
         $valididconfigobject2 = [
                 'modelid' => $tobedeletedmodelid,
         ];
         $existingconfigid2 = $DB->insert_record('tool_laaudit_model_configs', $valididconfigobject2);
+
+        // Store existing modelids for later, before deleting the last one
+        $existingmodelids = $DB->get_fieldset_select('analytics_models', 'id', '1=1');
         $DB->delete_records('analytics_models', ['id' => $tobedeletedmodelid]);
+
+        // Get a valid config id but with a non-existing model.
+        $falsemodelid = max($existingmodelids) + 1;
+        $invalididconfigobject = [
+                'modelid' => $falsemodelid,
+        ];
+        $existingconfigid3 = $DB->insert_record('tool_laaudit_model_configs', $invalididconfigobject);
 
         return [
                 'existingconfigexistingmodel' => [
                         'modelid' => $validmodelid,
                         'configid' => $existingconfigid,
+                        'throws' => false,
                 ],
                 'newconfigexistingmodel' => [
                         'modelid' => $validmodelid2,
                         'configid' => null,
+                        'throws' => false,
+                ],
+                'newconfigfalsemodel' => [
+                        'modelid' => $falsemodelid,
+                        'configid' => $existingconfigid3,
+                        'throws' => true,
                 ],
                 'existingconfigdeletedmodel' => [
                         'modelid' => $tobedeletedmodelid,
                         'configid' => $existingconfigid2,
+                        'throws' => false,
                 ],
         ];
     }
@@ -95,14 +115,17 @@ class model_configuration_get_or_create_and_get_for_model_test extends \advanced
      * @param int $modelid
      * @param int|null $configid
      */
-    public function test_model_configuration_get_or_create_and_get_for_model(int $modelid, ?int $configid) {
+    public function test_model_configuration_get_or_create_and_get_for_model(int $modelid, ?int $configid, bool $throws) {
         $this->resetAfterTest(true);
+        global $DB;
 
-        if (isset($configid)) {
+        if ($throws) {
+            $this->expectException(\Exception::class); // No config is referenced or created.
+            model_configuration::get_or_create_and_get_for_model($modelid);
+        } else if (isset($configid)) {
             $returnedconfigid = model_configuration::get_or_create_and_get_for_model($modelid);
             $this->assertEquals($configid, $returnedconfigid); // An existing config is referenced.
         } else {
-            global $DB;
             $existingconfigids = $DB->get_fieldset_select('tool_laaudit_model_configs', 'id', '1=1');
             $maxidbeforenewconfigcreation = (sizeof($existingconfigids) > 0) ? max($existingconfigids) : 0;
             $returnedconfigid = model_configuration::get_or_create_and_get_for_model($modelid);

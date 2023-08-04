045a3fc6a4dda59108abc2bb9bcfccbee9241b65
Linda Fernsel <linda.fernsel@htw-berlin.de>
2023-08-04 14:00:42 +0200

    fix version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_lala';
-$plugin->release = '1.0.1';
+$plugin->release = '2.0.0';
 $plugin->version = 2023080400;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;

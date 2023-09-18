21eb48b973c2a61d057b47fcda29525d204935d8
Linda Fernsel <linda.fernsel@htw-berlin.de>
2023-09-18 12:58:32 +0200

    import class that was not found in the unit test

--- tests/dataset_helper_test.php
+++ tests/dataset_helper_test.php
@@ -18,6 +18,8 @@ namespace tool_lala;
 
 use advanced_testcase;
 
+require_once(__DIR__ . '/fixtures/test_dataset_evidence.php');
+
 /**
  * Dataset helper test.
  *
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_lala';
-$plugin->release = '3.0.0';
-$plugin->version = 2023090600;
+$plugin->release = '3.0.1';
+$plugin->version = 2023090603;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;

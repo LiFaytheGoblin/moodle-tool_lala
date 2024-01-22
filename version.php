5d3cd5412a215887da71cfa7a3bd6891678a1558
Linda Fernsel <linda.fernsel@htw-berlin.de>
2024-01-22 13:22:07 +0100

    bump version in code

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_lala';
-$plugin->release = '3.1.0';
-$plugin->version = 2023110213;
+$plugin->release = '4.0.0';
+$plugin->version = 2024012213;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;
